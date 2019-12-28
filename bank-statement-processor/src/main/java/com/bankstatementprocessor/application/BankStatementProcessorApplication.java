58d58c52916661ec8812c2b7b6e33d3532b597ac
rahuljsr88 <56717590+rahuljsr88@users.noreply.github.com>
2019-12-28 11:05:48 +0100

    Add files via upload
    Initial project commit

---
+++ bank-statement-processor/src/main/java/com/bankstatementprocessor/application/BankStatementProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.bankstatementprocessor.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStatementProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStatementProcessorApplication.class, args);
+	}
+
+}
---
+++ bank-statement-processor/src/test/java/com/bankstatementprocessor/application/BankStatementProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankstatementprocessor.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankStatementProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
